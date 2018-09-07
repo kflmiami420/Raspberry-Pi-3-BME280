608b009b818e65339550c83ab79b713fb5952627
kflmiami420 <42599019+kflmiami420@users.noreply.github.com>
2018-09-07 12:16:26 -0400

    Update bme280twittertest1.py

--- bme280twittertest1.py
+++ bme280twittertest1.py
@@ -22,11 +22,32 @@ from auth import (consumer_key, consumer_secret, access_token, access_token_secr
             print("Twython did not tweet")
             log("Twython did not tweet")
 
-        print("Sleeping for 15 minutes")
-        sleep(900)
+#        print("Sleeping for 15 minutes")
+#        sleep(900)
 
 # Twitter detects repetition, so timestamp the tweets to prevent them reading as duplicates
-def get_timestamp():
-    timestamp = strftime("%Y%m%d-%H:%M:%S")
+#def get_timestamp():
+#    timestamp = strftime("%Y%m%d-%H:%M:%S")
 
-    return timestamp
+#    return timestamp
+
+
+#!/usr/bin/env python
+import os
+from twython import Twython
+
+
+CONSUMER_KEY = 'H2OaDt1XW52JsmFrlPOzr6EbA'
+CONSUMER_SECRET = 'F7slLnT5ovKMowFLhSsrhhTM8kErWpLDrGngwahizaDRtNXrgR'
+ACCESS_KEY = '1017199136121217026-fX37Uf2m7hXcuLnD9ekdFCrL1znxdz'
+ACCESS_SECRET = 'Op1lMH6sS6GRYMaRmeyGlCvKmc0sXKCWDz9ezNS4NySwe'
+
+api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+
+cmd = '/opt/vc/bin/vcgencmd measure_temp'
+line = os.popen(cmd).readline().strip()
+temp = line.split('=')[1].split("'")[0]
+api.update_status(status='My CPU temperature is '+temp+' C')
+print (sleeping for 15 minutes)
+sleep 900
+
