355881ae38427a19f37e9cedc890ae010f4c794f
kflmiami420 <42599019+kflmiami420@users.noreply.github.com>
2018-11-24 14:27:09 -0500

    Create bme280test.py

---
+++ bme280test.py
@@ -0,0 +1,18 @@
+
+import board
+import digitalio
+import busio
+import time
+import adafruit_bme280
+
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+bme280.sea_level_pressure = 1013.25
+
+while True:
+    print("\nTemperature: %0.1f C" % bme280.temperature)
+    print("Humidity: %0.1f %%" % bme280.humidity)
+    print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("Altitude = %0.2f meters" % bme280.altitude)
+    time.sleep(2)
